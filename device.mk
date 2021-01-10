0c6a15582eec828deb217bc2c46b05bbeab0fd42
Gagan Malvi <malvi@aospa.co>
2021-01-10 15:49:50 +0530

    onc: Build basic USB HAL
    Signed-off-by: Gagan Malvi <malvi@aospa.co>

--- device.mk
+++ device.mk
@@ -408,7 +408,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.onclite
+    android.hardware.usb@1.0-service.basic
 
 # Vibrator
 PRODUCT_PACKAGES += \
